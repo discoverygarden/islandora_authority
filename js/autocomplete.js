082b752e364b34387995c137e93f5085a592c79a
Adam Vessey <adam@discoverygarden.ca>
2014-07-16 21:48:04 +0000

    A fair ways in?
    Seems to be some differences in the way D7 caches forms, though...

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -3,15 +3,15 @@
 /**
  * Attaches the autocomplete behavior to all required fields.
  */
-Drupal.behaviors.autocomplete = {
+Drupal.behaviors.islandora_authority_autocomplete = {
   attach: function (context, settings) {
     var acdb = [];
-    $('input.autocomplete', context).once('autocomplete', function () {
+    $('input.islandora-authority-autocomplete', context).once('islandora-authority-autocomplete', function () {
       var uri = this.value;
       if (!acdb[uri]) {
         acdb[uri] = new Drupal.ACDB(uri);
       }
-      var $input = $('#' + this.id.substr(0, this.id.length - 13))
+      var $input = $('#' + this.id.substr(0, this.id.length - 33))
         .attr('autocomplete', 'OFF')
         .attr('aria-autocomplete', 'list');
       $($input[0].form).submit(Drupal.autocompleteSubmit);
@@ -20,41 +20,20 @@ Drupal.behaviors.autocomplete = {
         .append($('<span class="element-invisible" aria-live="assertive"></span>')
           .attr('id', $input.attr('id') + '-autocomplete-aria-live')
         );
-      new Drupal.jsAC($input, acdb[uri]);
+      new Drupal.islandora_authority_jsAC($input, acdb[uri]);
     });
   }
 };
 
 /**
- * Prevents the form from submitting if the suggestions popup is open
- * and closes the suggestions popup when doing so.
- */
-Drupal.autocompleteSubmit = function () {
-  return $('#autocomplete').each(function () {
-    this.owner.hidePopup();
-  }).length == 0;
-};
-
-/**
  * An AutoComplete object.
  */
-Drupal.jsAC = function ($input, db) {
-  var ac = this;
-  this.input = $input[0];
-  this.ariaLive = $('#' + this.input.id + '-autocomplete-aria-live');
-  this.db = db;
-
-  $input
-    .keydown(function (event) { return ac.onkeydown(this, event); })
-    .keyup(function (event) { ac.onkeyup(this, event); })
-    .blur(function () { ac.hidePopup(); ac.db.cancel(); });
-
-};
+Drupal.islandora_authority_jsAC = Drupal.jsAC;
 
 /**
  * Handler for the "keydown" event.
  */
-Drupal.jsAC.prototype.onkeydown = function (input, e) {
+Drupal.islandora_authority_jsAC.prototype.onkeydown = function (input, e) {
   if (!e) {
     e = window.event;
   }
@@ -65,46 +44,13 @@ Drupal.jsAC.prototype.onkeydown = function (input, e) {
     case 38: // up arrow.
       this.selectUp();
       return false;
+    case 37:
+      this.selectLeft();
+      return false;
+    case 39:
+      this.selectRight();
+      return false;
     default: // All other keys.
-      return true;
-  }
-};
-
-/**
- * Handler for the "keyup" event.
- */
-Drupal.jsAC.prototype.onkeyup = function (input, e) {
-  if (!e) {
-    e = window.event;
-  }
-  switch (e.keyCode) {
-    case 16: // Shift.
-    case 17: // Ctrl.
-    case 18: // Alt.
-    case 20: // Caps lock.
-    case 33: // Page up.
-    case 34: // Page down.
-    case 35: // End.
-    case 36: // Home.
-    case 37: // Left arrow.
-    case 38: // Up arrow.
-    case 39: // Right arrow.
-    case 40: // Down arrow.
-      return true;
-
-    case 9:  // Tab.
-    case 13: // Enter.
-    case 27: // Esc.
-      this.hidePopup(e.keyCode);
-      return true;
-
-    default: // All other keys.
-      if (input.value.length > 0 && !input.readOnly) {
-        this.populatePopup();
-      }
-      else {
-        this.hidePopup(e.keyCode);
-      }
       return true;
   }
 };
@@ -112,15 +58,42 @@ Drupal.jsAC.prototype.onkeyup = function (input, e) {
 /**
  * Puts the currently highlighted suggestion into the autocomplete field.
  */
-Drupal.jsAC.prototype.select = function (node) {
-  this.input.value = $(node).data('autocompleteValue');
+Drupal.islandora_authority_jsAC.prototype.select = function (node) {
+  var obj = $(node).data('autocompleteSet');
+  var parents = this.input.id.split('--');
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function' &&
+      prop != 'full-display' && prop != 'alts') {
+      //slice to get rid of the name of the current field
+      var id_parts = parents.slice(0, parents.length - 1);
+
+      //Add the part which the current property represents, while making the property
+      // name match what should have been put into the id, so we can select the
+      // relevant field below.
+      id_parts.push(prop.replace(/(\]\[|_| )/g, '-'));
+
+      //Update the contents of the required field.
+      $('#'+id_parts.join('--')).val(obj[prop]);
+    }
+  }
+  this.unhighlightSub(this.sub_selected);
+  this.unhighlight(this.selected);
 };
 
 /**
  * Highlights the next suggestion.
  */
-Drupal.jsAC.prototype.selectDown = function () {
-  if (this.selected && this.selected.nextSibling) {
+Drupal.islandora_authority_jsAC.prototype.selectDown = function () {
+  if (this.sub_selected && this.sub_selected.nextSibling) {
+    this.sub_highlight(this.sub_selected.nextSibling);
+  }
+  else if (this.sub_popup) {
+    var lis = $('li', this.sub_popup);
+    if (lis.length > 0) {
+      this.sub_highlight(lis.get(0));
+    }
+  }
+  else if (this.selected && this.selected.nextSibling) {
     this.highlight(this.selected.nextSibling);
   }
   else if (this.popup) {
@@ -134,8 +107,11 @@ Drupal.jsAC.prototype.selectDown = function () {
 /**
  * Highlights the previous suggestion.
  */
-Drupal.jsAC.prototype.selectUp = function () {
-  if (this.selected && this.selected.previousSibling) {
+Drupal.islandora_authority_jsAC.prototype.selectUp = function () {
+  if (this.sub_selected && this.sub_selected.previousSibling) {
+    this.sub_highlight(this.sub_selected.previousSibling);
+  }
+  else if (this.selected && this.selected.previousSibling) {
     this.highlight(this.selected.previousSibling);
   }
 };
@@ -143,46 +119,63 @@ Drupal.jsAC.prototype.selectUp = function () {
 /**
  * Highlights a suggestion.
  */
-Drupal.jsAC.prototype.highlight = function (node) {
-  if (this.selected) {
-    $(this.selected).removeClass('selected');
+Drupal.islandora_authority_jsAC.prototype.highlight = function (node) {
+  Drupal.jsAC.highlight.call(this, node);
+  this.showSubmenu(this.selected);
+};
+Drupal.islandora_authority_jsAC.prototype.sub_highlight = function (node) {
+  if (this.sub_selected) {
+    $(this.sub_selected).removeClass('selected');
   }
   $(node).addClass('selected');
-  this.selected = node;
-  $(this.ariaLive).html($(this.selected).html());
+  this.sub_selected = node;
+  $(this.ariaLive).html($(this.sub_selected).html());
 };
 
 /**
  * Unhighlights a suggestion.
  */
-Drupal.jsAC.prototype.unhighlight = function (node) {
+Drupal.islandora_authority_jsAC.prototype.sub_unhighlight = function (node) {
   $(node).removeClass('selected');
-  this.selected = false;
+  this.sub_selected = false;
   $(this.ariaLive).empty();
 };
 
 /**
  * Hides the autocomplete suggestions.
  */
-Drupal.jsAC.prototype.hidePopup = function (keycode) {
+Drupal.islandora_authority_jsAC.prototype.hidePopup = function (keycode) {
   // Select item if the right key or mousebutton was pressed.
-  if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
-    this.input.value = $(this.selected).data('autocompleteValue');
+  if (((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+    if (this.sub_selected) {
+      this.select(this.sub_selected);
+    }
+    else if (this.selected) {
+      this.select(this.selected);
+    }
   }
+
   // Hide popup.
+  var sub_popup = this.sub_popup;
+  if (sub_popup) {
+    this.sub_popup = null;
+    $(sub_popup).fadeOut('fast', function () { $(sub_popup).remove(); });
+  }
   var popup = this.popup;
   if (popup) {
     this.popup = null;
     $(popup).fadeOut('fast', function () { $(popup).remove(); });
   }
+
   this.selected = false;
+  this.sub_selected = false;
   $(this.ariaLive).empty();
 };
 
 /**
  * Positions the suggestions popup and starts a search.
  */
-Drupal.jsAC.prototype.populatePopup = function () {
+Drupal.islandora_authority_jsAC.prototype.populatePopup = function () {
   var $input = $(this.input);
   var position = $input.position();
   // Show popup.
@@ -190,7 +183,7 @@ Drupal.jsAC.prototype.populatePopup = function () {
     $(this.popup).remove();
   }
   this.selected = false;
-  this.popup = $('<div id="autocomplete"></div>')[0];
+  this.popup = $('<div id="islandora-authority-autocomplete"></div>')[0];
   this.popup.owner = this;
   $(this.popup).css({
     top: parseInt(position.top + this.input.offsetHeight, 10) + 'px',
@@ -205,10 +198,28 @@ Drupal.jsAC.prototype.populatePopup = function () {
   this.db.search(this.input.value);
 };
 
+Drupal.islandora_authority_jsAC.prototype.showSubmenu = function (node) {
+  // Show popup
+  if (this.sub_popup) {
+    $(this.sub_popup).hide();
+  }
+  this.sub_selected = false;
+  if (typeof node.alt_popup != 'undefined') {
+    this.sub_popup = node.alt_popup;
+    $(this.sub_popup)
+      .css({
+        marginLeft: (node.offsetWidth - 4) +'px',
+        width: (node.offsetWidth) +'px',
+        top: (node.offsetTop) + 'px'
+      })
+      .show();
+  }
+};
+
 /**
  * Fills the suggestion popup with any matches received.
  */
-Drupal.jsAC.prototype.found = function (matches) {
+Drupal.islandora_authority_jsAC.prototype.found = function (matches) {
   // If no value in the textfield, do not show the popup.
   if (!this.input.value.length) {
     return false;
@@ -218,13 +229,33 @@ Drupal.jsAC.prototype.found = function (matches) {
   var ul = $('<ul></ul>');
   var ac = this;
   for (key in matches) {
-    $('<li></li>')
-      .html($('<div></div>').html(matches[key]))
+    var li = $('<li></li>')
+      .html($('<div></div>').html(matches[key]['full-display']))
       .mousedown(function () { ac.select(this); })
       .mouseover(function () { ac.highlight(this); })
       .mouseout(function () { ac.unhighlight(this); })
-      .data('autocompleteValue', key)
+      .data('autocompleteSet', matches[key])
       .appendTo(ul);
+    var alt_ul = $('<ul></ul>');
+    for (var prop in obj['alts']) {
+      $('<li></li>')
+        .html($('<div></div>').html(matches[key]['alts'][prop]['full-display']))
+        .mousedown(function (event) {
+          if (event.which == 1) {
+            ac.select(this);
+          }
+          event.stopPropagation();
+        })
+        .mouseover(function () { ac.sub_highlight(this); })
+        .mouseout(function () { ac.sub_unhighlight(this); })
+        .data('autocompleteSet', matches[key]['alts'][prop])
+        .appendTo(alt_ul)
+        .hide();
+    }
+    if (alt_ul.childNodes.length > 0) {
+      li.alt_popup = alt_ul;
+      $(li).append(alt_ul);
+    }
   }
 
   // Show popup with matches, if any.
@@ -238,87 +269,6 @@ Drupal.jsAC.prototype.found = function (matches) {
       this.hidePopup();
     }
   }
-};
-
-Drupal.jsAC.prototype.setStatus = function (status) {
-  switch (status) {
-    case 'begin':
-      $(this.input).addClass('throbbing');
-      $(this.ariaLive).html(Drupal.t('Searching for matches...'));
-      break;
-    case 'cancel':
-    case 'error':
-    case 'found':
-      $(this.input).removeClass('throbbing');
-      break;
-  }
-};
-
-/**
- * An AutoComplete DataBase object.
- */
-Drupal.ACDB = function (uri) {
-  this.uri = uri;
-  this.delay = 300;
-  this.cache = {};
-};
-
-/**
- * Performs a cached and delayed search.
- */
-Drupal.ACDB.prototype.search = function (searchString) {
-  var db = this;
-  this.searchString = searchString;
-
-  // See if this string needs to be searched for anyway.
-  searchString = searchString.replace(/^\s+|\s+$/, '');
-  if (searchString.length <= 0 ||
-    searchString.charAt(searchString.length - 1) == ',') {
-    return;
-  }
-
-  // See if this key has been searched for before.
-  if (this.cache[searchString]) {
-    return this.owner.found(this.cache[searchString]);
-  }
-
-  // Initiate delayed search.
-  if (this.timer) {
-    clearTimeout(this.timer);
-  }
-  this.timer = setTimeout(function () {
-    db.owner.setStatus('begin');
-
-    // Ajax GET request for autocompletion. We use Drupal.encodePath instead of
-    // encodeURIComponent to allow autocomplete search terms to contain slashes.
-    $.ajax({
-      type: 'GET',
-      url: db.uri + '/' + Drupal.encodePath(searchString),
-      dataType: 'json',
-      success: function (matches) {
-        if (typeof matches.status == 'undefined' || matches.status != 0) {
-          db.cache[searchString] = matches;
-          // Verify if these are still the matches the user wants to see.
-          if (db.searchString == searchString) {
-            db.owner.found(matches);
-          }
-          db.owner.setStatus('found');
-        }
-      },
-      error: function (xmlhttp) {
-        alert(Drupal.ajaxError(xmlhttp, db.uri));
-      }
-    });
-  }, this.delay);
-};
-
-/**
- * Cancels the current autocomplete request.
- */
-Drupal.ACDB.prototype.cancel = function () {
-  if (this.owner) this.owner.setStatus('cancel');
-  if (this.timer) clearTimeout(this.timer);
-  this.searchString = '';
 };
 
 })(jQuery);
